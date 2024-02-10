bcafd12a5b03546042d701e575c0920df2e213a3
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-02-09 20:09:48 -0700

    add stable code without implicit waits

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/AdminPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/AdminPage.java
@@ -1,29 +1,15 @@
 package com.c4hco.test.automation.pages.cocoAndExchangeCommonPages;
 
-import com.c4hco.test.automation.dbUtils.PostgresStatementExecutor;
-import com.c4hco.test.automation.utils.ApplicationProperties;
 import com.c4hco.test.automation.utils.BasicActions;
-import com.c4hco.test.automation.utils.Utils;
-import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.time.Duration;
-
 public class AdminPage {
-    CreateAccountPage createAccountPage = new CreateAccountPage();
     private BasicActions basicActions;
-    private Utils utils = new Utils();
-    private PostgresStatementExecutor executor = new PostgresStatementExecutor();
-    private String env = ApplicationProperties.getInstance().getProperty("env");
     public AdminPage(){
         this.basicActions = BasicActions.getInstance();
         PageFactory.initElements(basicActions.getDriver(), this);
-    }
-
-    public void closeBrowser(){
-        basicActions.getDriver().close();
     }
 
     @FindBy(xpath = "//div[@class='form-btn-group big-screen']//button[@id='create-individual-account']")
@@ -36,7 +22,6 @@ public class AdminPage {
     public WebElement logoutAdmin;
 
     public void logoutFromAdmin() {
-        basicActions.implicitWait(2000);
         navigateToPreviousPage();
         basicActions.waitForElementToBePresent(dropdownArrow,100);
         dropdownArrow.click();
