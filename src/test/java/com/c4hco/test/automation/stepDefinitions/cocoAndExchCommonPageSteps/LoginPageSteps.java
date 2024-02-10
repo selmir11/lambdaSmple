60a5a1bbd6d8bed34a1f1d40ddf5a2b1bcb8f712
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-02-09 21:46:04 -0700

    format

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/LoginPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/LoginPageSteps.java
@@ -1,40 +1,23 @@
 package com.c4hco.test.automation.stepDefinitions.cocoAndExchCommonPageSteps;
 
-import com.c4hco.test.automation.pages.cocoAndExchangeCommonPages.CreateAccountPage;
-import com.c4hco.test.automation.utils.BasicActions;
-import com.c4hco.test.automation.utils.Utils;
-import io.cucumber.java.en.*;
 import com.c4hco.test.automation.pages.cocoAndExchangeCommonPages.LoginPage;
-
-import java.time.Duration;
-
-
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 
 public class LoginPageSteps {
 
         LoginPage loginPage = new LoginPage();
-        BasicActions basicActions = new BasicActions();
-
-        CreateAccountPage createAccountPage = new CreateAccountPage();
-
+        
         @When("I click create a new account on login page")
-            public void iClickCreateAccount(){
-            loginPage.clickCreateAccount();
-        }
+            public void iClickCreateAccount(){ loginPage.clickCreateAccount();}
 
         @And("I enter valid credentials to login")
-
            public void iEnterValidCredentials(){
             loginPage.logInWithValidCredentials();
         }
 
-
-
-    @When("I login as Admin User")
-    public void i_login_as_admin_user() {
-        loginPage.loginAsAnAdminUser();
-
-    }
+         @When("I login as Admin User")
+            public void i_login_as_admin_user() {loginPage.loginAsAnAdminUser();}
 
         // =================VALIDATION STEPS==============//
     }
