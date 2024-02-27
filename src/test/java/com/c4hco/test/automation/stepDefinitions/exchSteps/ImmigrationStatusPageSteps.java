a6af68bf68ae1b5430a59708b6a5da001a025c94
malukianenko <mlukianenko@c4hco.com>
2024-02-27 14:14:50 -0700

    updates

--- src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/ImmigrationStatusPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/ImmigrationStatusPageSteps.java
@@ -1,11 +1,12 @@
 package com.c4hco.test.automation.stepDefinitions.exchSteps;
 
 import com.c4hco.test.automation.pages.exchPages.ImmigrationStatusPage;
+import com.c4hco.test.automation.utils.WebDriverManager;
 import io.cucumber.java.en.Then;
 
 public class ImmigrationStatusPageSteps {
 
-    ImmigrationStatusPage ImmigrationStatusPage = new ImmigrationStatusPage();
+    ImmigrationStatusPage ImmigrationStatusPage = new ImmigrationStatusPage(WebDriverManager.getDriver());
 
     @Then("I select {string} for Lawful Permanent Resident")
     public void isMemberLawfulPermanentResident(String YNLawfulPermanentResident) {
