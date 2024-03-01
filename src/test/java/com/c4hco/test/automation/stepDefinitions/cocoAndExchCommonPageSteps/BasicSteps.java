a4fa38e4ff2186a50a6ea3f450879406c7a51b64
Ravi Stephens <rstephens@c4hco.com>
2024-03-01 12:23:06 -0700

    SLER-80 Complete
    This test is complete and it's a conversion of test RT1973 compare dental plans.

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/BasicSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/BasicSteps.java
@@ -111,6 +111,8 @@ public class BasicSteps {
                 break;
             case "Tell us about life changes":
                 pageUrl = "nes/qlceConfirmation";
+            case "dentalCompare":
+                pageUrl = "dentalCompare";
                 break;
             default:
                 System.out.println("undefined page ");
