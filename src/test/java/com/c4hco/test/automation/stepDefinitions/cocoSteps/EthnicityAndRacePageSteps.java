7982080e4aff082c9a1827722dba38cf3904bde9
malukianenko <mlukianenko@c4hco.com>
2024-03-04 15:47:30 -0700

    SLCR-24

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/EthnicityAndRacePageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/EthnicityAndRacePageSteps.java
@@ -3,6 +3,7 @@ package com.c4hco.test.automation.stepDefinitions.cocoSteps;
 import com.c4hco.test.automation.pages.cocoPages.EthnicityAndRacePage;
 import com.c4hco.test.automation.utils.WebDriverManager;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 
 public class EthnicityAndRacePageSteps {
     EthnicityAndRacePage ethnicityAndRacePage = new EthnicityAndRacePage(WebDriverManager.getDriver());
@@ -15,4 +16,7 @@ public class EthnicityAndRacePageSteps {
 
     @And("I click continue on the Ethnicity and race page")
     public void clickContinue(){ethnicityAndRacePage.clickSaveAndContinueButton();}
+
+    @Then("I verify {string} text on the Ethnicity And Race page")
+    public void verifyEthnicityAndRacePageText(String Language)  { ethnicityAndRacePage.verifyTextOnEthnicityAndRace(Language); }
 }
