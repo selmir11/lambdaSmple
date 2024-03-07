c51d3074a605b679762c47b910983145751dbb70
marcigreene <77079569+marcigreene@users.noreply.github.com>
2024-03-07 12:20:55 -0700

    Update HeaderAndFooterPage.java

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/HeaderAndFooterPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/HeaderAndFooterPage.java
@@ -117,15 +117,15 @@ public class HeaderAndFooterPage {
     public void clickApplyForCoverageLink(String pageType) {
         switch (pageType){
             case "Elmo":
-                basicActions.waitForElementListToBePresent(centerHeaderLink, 9999);
+                basicActions.waitForElementListToBePresent(centerHeaderLink, 15);
                 centerHeaderLink.get(0).click();
                 break;
             case "NonElmo":
-                basicActions.waitForElementListToBePresent(centerHeaderLinkNonElmo, 9999);
+                basicActions.waitForElementListToBePresent(centerHeaderLinkNonElmo, 15);
                 centerHeaderLinkNonElmo.get(0).click();
                 break;
             case "ExpertHelp":
-                basicActions.waitForElementListToBePresent(centerHeaderLinkExpertHelp, 9999);
+                basicActions.waitForElementListToBePresent(centerHeaderLinkExpertHelp, 15);
                 centerHeaderLinkExpertHelp.get(0).click();
                 break;
             default:
@@ -133,19 +133,19 @@ public class HeaderAndFooterPage {
         }
     }
     public void clickFindAPlanLinkLink() {
-        basicActions.waitForElementListToBePresent(centerHeaderLink, 9999);
+        basicActions.waitForElementListToBePresent(centerHeaderLink, 15);
         centerHeaderLink.get(1).click();
     }
     public void clickMyAccountLink() {
-        basicActions.waitForElementListToBePresent(centerHeaderLink, 9999);
+        basicActions.waitForElementListToBePresent(centerHeaderLink, 15);
         centerHeaderLink.get(2).click();
     }
     public void clickLearnMoreLink() {
-        basicActions.waitForElementToBeClickable(learnMoreLink, 9999);
+        basicActions.waitForElementToBeClickable(learnMoreLink, 15);
         learnMoreLink.click();
     }
     public void clickGetAssistanceLink() {
-        basicActions.waitForElementToBeClickable(getAssistanceLink, 9999);
+        basicActions.waitForElementToBeClickable(getAssistanceLink, 15);
         getAssistanceLink.click();
     }
     public void clickFindExpertAssistanceLink(String pageType) {
