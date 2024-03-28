1c08dc4e9d458c4e38cd6dff84b28d7eac4011da
marcigreene <77079569+marcigreene@users.noreply.github.com>
2024-03-28 07:52:06 -0600

    Updates

--- src/main/java/com/c4hco/test/automation/pages/exchPages/LawfulPresencePage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/LawfulPresencePage.java
@@ -12,7 +12,6 @@ import java.util.Random;
 
 public class LawfulPresencePage {
     private BasicActions basicActions;
-   // String PrimaryMemberId = "";
 
     public LawfulPresencePage(WebDriver webDriver) {
         basicActions = new BasicActions(webDriver);
