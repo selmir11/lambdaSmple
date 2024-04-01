d0c6d876384ea343dd9ab6ff5bc79b3e4085f63a
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-04-01 12:23:39 -0600

    optimize file download

--- src/test/java/com/c4hco/test/automation/stepDefinitions/sftpSteps/sftpStepDefinitions.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/sftpSteps/sftpStepDefinitions.java
@@ -7,7 +7,6 @@ import com.jcraft.jsch.JSchException;
 import io.cucumber.java.en.And;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,42 +15,21 @@ public class sftpStepDefinitions {
 
     @And("I download the file(s) from sftp server with location {string}")
     public void downloadFiles(String remoteLocation) throws JSchException {
-
-        // TO DO:: This file name validation can fo to DB Validations - see if we do not need to save entity in shared data.
-        // Set Medical file name and dental fileName in SharedData - Later can change it to List.
-
-        Map<String, String> insCodeAndFileName = new HashMap<>();;
+        List<String> ediFileNames = new ArrayList<>();
 
       List<Ob834DetailsEntity> ob834Entries =  SharedData.getOb834DetailsEntities();
       for(Ob834DetailsEntity entry: ob834Entries){
-          System.out.println("entry ---::"+entry);
-         System.out.println("Insurance line code "+ entry.getInsurance_line_code());
-                System.out.println("fileName from entry--"+entry.getFilename());
-          insCodeAndFileName.put(entry.getInsurance_line_code(), entry.getFilename());
+         if(entry.getInsurance_line_code().equals("HLT")){
+             SharedData.setMedicalFileName(entry.getFilename());
+         } else if(entry.getInsurance_line_code().equals("DEN")) {
+             SharedData.setDentalFileName(entry.getFilename());
+         }
+          ediFileNames.add(entry.getFilename());
       }
 
-      String medicalFileName = insCodeAndFileName.get("HLT");
-      String dentalFileName = insCodeAndFileName.get("DEN");
-
-      SharedData.setMedicalFileName(medicalFileName);
-      SharedData.setDentalFileName(dentalFileName);
-
-
-      List<String> fileNames = new ArrayList<>();
-
-      fileNames.add(medicalFileName);
-      fileNames.add(dentalFileName);
-      System.out.println("--med file name--"+medicalFileName);
-        System.out.println("--dental file name--"+dentalFileName);
-
-        System.out.println("filenNEMA--"+fileNames);
-
-        for(String filename: fileNames){
-          System.out.println("fileName--"+filename);
+        for(String filename: ediFileNames){
           sftpUtil.downloadFileWithSftp(remoteLocation, filename);
-
       }
-
 
     }
 
