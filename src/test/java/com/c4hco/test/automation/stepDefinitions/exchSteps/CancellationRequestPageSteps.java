58a1609b9b89b57915756051061f37ab7fcec7b3
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2024-04-29 20:16:36 +0530

    Updated Script for CCRT232
    Updated Script for CCRT232

--- src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/CancellationRequestPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/CancellationRequestPageSteps.java
@@ -37,7 +37,7 @@ public class CancellationRequestPageSteps {
     }
 
     @Then("I validate Confirmation popup message displayed in expected Language")
-    public void i_validate_confirmation_popup_message_displayed_in_expected_language(List<String> data) throws InterruptedException {
+    public void validateConfirmationPpopupInExpectedLanguage(List<String> data) throws InterruptedException {
         cancellationRequestPage.cancellationPlanPoupMessage(data);
     }
 
