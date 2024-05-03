1d221401288e1425b686a5b1ec96f23921892069
marcigreene <77079569+marcigreene@users.noreply.github.com>
2024-05-03 14:44:29 -0600

    Seperated Elmo and Non-Elmo
    •	Moved all Elmo methods/elements to _Elmo page/step
    o	exchPages\OhiRetireeHealthPlanPage
    o	exchSteps\OhiRetireeHealthPlanSteps
    o	exchPages\OhiVaHealthcarePage
    o	exchSteps\OhiVaHealthcareSteps
    o	exchPages\OhiMedicarePage
    o	exchSteps\OhiMedicareSteps
    o	exchPages\OhiIndividualInsurancePage
    o	exchSteps\OhiIndividualInsuranceSteps
    •	Created new _Elmo page/step
    o	exchPages\OhiRetireeHealthPlanPage_Elmo
    o	exchSteps\OhiRetireeHealthPlanSteps_Elmo
    o	exchPages\OhiVaHealthcarePage_Elmo
    o	exchSteps\OhiVaHealthcareSteps_Elmo
    o	exchPages\OhiMedicarePage_Elmo
    o	exchSteps\OhiMedicareSteps_Elmo
    o	exchPages\OhiIndividualInsurancePage_Elmo
    o	exchSteps\OhiIndividualInsuranceSteps_Elmo

---
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/OhiRetireeHealthPlanSteps_Elmo.java
@@ -0,0 +1,65 @@
+package com.c4hco.test.automation.stepDefinitions.exchSteps;
+
+import com.c4hco.test.automation.pages.exchPages.OhiRetireeHealthPlanPage_Elmo;
+import com.c4hco.test.automation.utils.WebDriverManager;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class OhiRetireeHealthPlanSteps_Elmo {
+    OhiRetireeHealthPlanPage_Elmo ohiRetireeHealthPlanPage_Elmo = new OhiRetireeHealthPlanPage_Elmo(WebDriverManager.getDriver());
+
+    @Then("I click continue on the Elmo OHI Retiree page")
+    public void iClickSaveAndContinue(){ohiRetireeHealthPlanPage_Elmo.clickSaveAndContinue();}
+
+    @Then("I click {string} for currently enrolled in Retiree question")
+    public void iClickCurrentlyEnrolledOption(String currentlyEnrolled){ohiRetireeHealthPlanPage_Elmo.clickCurrentlyEnrolledOption(currentlyEnrolled);}
+
+    @Then("I click {string} for insurance ending in 60 days in Retiree question")
+    public void iClickInsuranceEndingOption(String insuranceEnding){ohiRetireeHealthPlanPage_Elmo.clickInsuranceEndingOption(insuranceEnding);}
+
+    @Then("I enter end date of end of current month for Retiree")
+    public void iEnterEndDate(){ohiRetireeHealthPlanPage_Elmo.enterEndDate();}
+
+    @Then("I click {string} for insurance ending voluntary for Retiree question")
+    public void iClickEndVoluntaryOption(String voluntaryEnding){ohiRetireeHealthPlanPage_Elmo.clickEndVoluntaryOption(voluntaryEnding);}
+
+
+
+
+
+
+
+
+
+
+
+
+    // ############################## VALIDATION STEPS #########################
+    // Add only validation methods below this line
+    @And("I verify the header for Primary Member on the Retiree page")
+    public void iVerifyTextOnOtherHealthInsurancePage(){ohiRetireeHealthPlanPage_Elmo.verifyHeadersRetireeOhiPageEnglish();}
+
+    @And("I verify Currently Enrolled is {string} on the Retiree page")
+    public void iVerifyCurrentlyEnrolledOption(String currentlyEnrolled){ohiRetireeHealthPlanPage_Elmo.verifyCurrentlyEnrolledOption(currentlyEnrolled);}
+
+    @And("I verify Insurance Ending in 60 days is {string} on the Retiree page")
+    public void iVerifyInsuranceEndingOption(String insuranceEnding){ohiRetireeHealthPlanPage_Elmo.verifyInsuranceEndingOption(insuranceEnding);}
+
+    @And("I verify end date is the last day of the month on the Retiree page")
+    public void iVerifyEndDate(){ohiRetireeHealthPlanPage_Elmo.verifyEndDate();}
+
+    @And("I verify Voluntarily Ending is {string} on the Retiree page")
+    public void iVerifyVoluntaryEndingOption(String voluntaryEnding){ohiRetireeHealthPlanPage_Elmo.verifyVoluntaryEndingOption(voluntaryEnding);}
+
+    @And("I verify the {string} Retiree page data in {string}")
+    public void iVerifyRetireePageData(String dataToVerify, String language) {ohiRetireeHealthPlanPage_Elmo.verifyRetireePageData(dataToVerify, language);}
+
+    @And("I verify {string} error in {string} on the Retiree page")
+    public void iVerifyErrorMessage(String errorType, String language){ohiRetireeHealthPlanPage_Elmo.verifyErrorMessage(errorType, language);}
+
+    @And("I verify {string} error does not show for Retiree page")
+    public void iVerifyNoErrorMessage(String errorType)  {ohiRetireeHealthPlanPage_Elmo.verifyNoErrorMessage(errorType);}
+
+
+
+}
