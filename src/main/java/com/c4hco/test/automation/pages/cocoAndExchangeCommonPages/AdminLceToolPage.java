223603c94faeade506eb0ec9a2f116f3585e8ab4
exmatuszewska <53319154+exmatuszewska@users.noreply.github.com>
2024-05-06 14:32:16 -0600

    Update AdminLceToolPage.java

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/AdminLceToolPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/AdminLceToolPage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
-
 import java.util.ArrayList;
 import java.util.List;
 
