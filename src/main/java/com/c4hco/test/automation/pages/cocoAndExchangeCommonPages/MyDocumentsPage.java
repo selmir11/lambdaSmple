155ae4f879f3575ededc0e05928e0dcc2ed99d17
GauriJog <gjog@c4hco.com>
2024-05-31 10:17:58 -0600

    SLCR-80 Completed and added locator change in ogin page
    Automated my documents Header Footer, validate go back to welcome page button and sign out functionality.

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/MyDocumentsPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/MyDocumentsPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
-
 public class MyDocumentsPage {
 
     private BasicActions basicActions;
@@ -26,16 +25,18 @@ public class MyDocumentsPage {
     @FindBy(css = ".documents-notices-title.header-2")
     WebElement myDocumentsSubTitle;
 
-    @FindBy(css = ".documents-notices-content-container > div")
+//    @FindBy(css = ".documents-notices-content-container > div")
+    @FindBy(css =".document-notice-name")
     WebElement documentsInfoMessage;
+
+    @FindBy(xpath = "//button[normalize-space()='Go back to Welcome page']")
+    WebElement goBackWelcomePage;
+
 
     public void ClickLinkMyDocsWelcomePage() {
         basicActions.switchToParentPage("accountOverview");
         accountOverviewPage.clickHereLinks("My Documents");
     }
-
-
-
 
                 //============================VALIDATION STEPS==============//
 
@@ -68,8 +69,14 @@ public class MyDocumentsPage {
      }
 
     public void validateTheNoticeExistInMyDocumentLetterPage(String documentName) {
+        basicActions.waitForElementToBePresent(documentsInfoMessage,100);
         basicActions.scrollToElement(documentsInfoMessage);
         softAssert.assertEquals(documentsInfoMessage.getText(),documentName);
         softAssert.assertAll();
+    }
+
+    public void goBackToWelcomePage(){
+        basicActions.waitForElementToBeClickable(goBackWelcomePage,30);
+        basicActions.click(goBackWelcomePage);
     }
 }
