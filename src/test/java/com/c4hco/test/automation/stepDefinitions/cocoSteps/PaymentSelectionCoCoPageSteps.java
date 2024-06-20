30cced8f1f8104f472c1031d39836994b8b43ce9
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2024-06-20 10:02:11 +0530

    Updated Script for SCRT-159
    Updated Script for SCRT-159

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/PaymentSelectionCoCoPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/PaymentSelectionCoCoPageSteps.java
@@ -7,6 +7,11 @@ import io.cucumber.java.en.And;
 public class PaymentSelectionCoCoPageSteps {
     PaymentSelectionCoCoPage paymentSelectionCoCoPage = new PaymentSelectionCoCoPage(WebDriverManager.getDriver());
 
+    @And("I select the Money Order button on the Payment Selection CoCo page")
+    public void clickMoneyOrderButton() {
+        paymentSelectionCoCoPage.clickMoneyOrderButton();
+    }
+
     @And("I select Continue button on the Payment Selection CoCo page")
     public void paymentSelectionCoCo(){
         paymentSelectionCoCoPage.paymentSelectionContinueCoCo();
