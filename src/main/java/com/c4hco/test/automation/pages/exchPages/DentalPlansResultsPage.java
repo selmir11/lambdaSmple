efa0f4dbf890d5f85cea2920638f002116b23bf4
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-07-16 10:51:59 -0600

    OBO - Plansummary hard stop

--- src/main/java/com/c4hco/test/automation/pages/exchPages/DentalPlansResultsPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/DentalPlansResultsPage.java
@@ -33,7 +33,7 @@ public class DentalPlansResultsPage {
     WebElement btnGoBack;
 
     @FindBy(id= "DentalPlanResults-Skip")
-    WebElement btnSkip;
+    WebElement dentalSkipBtn;
 
     @FindBy (id ="mat-mdc-checkbox-28-input")
     WebElement comparePlanBox1;
@@ -62,6 +62,9 @@ public class DentalPlansResultsPage {
     @FindBy(css = ".plan-results-container .responsive-text-align-left")
     WebElement dentalPlanCount;
 
+    @FindBy(css=".fas.fa-spinner.fa-spin")
+    WebElement spinner;
+
     SoftAssert softAssert = new SoftAssert();
 
     public void iGetFirstDentalPlanName() {
@@ -84,8 +87,10 @@ public class DentalPlansResultsPage {
 
     }
     public void clickSkip(){
-        basicActions.waitForElementToBePresent(btnSkip,30);
-        btnSkip.click();
+        basicActions.waitForElementToDisappear(spinner, 20);
+        basicActions.waitForElementToBePresent(dentalSkipBtn,30);
+      //  basicActions.scrollToElement(dentalSkipBtn);
+        dentalSkipBtn.click();
     }
     public void clickFirstTwoCompareBoxes() {
         basicActions.waitForElementToBePresent( comparePlanBox1,10 );
