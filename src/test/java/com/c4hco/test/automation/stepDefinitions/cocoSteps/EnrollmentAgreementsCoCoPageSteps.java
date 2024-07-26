53f159f2ad0e3bc6500b53dbddd189ce611b05e3
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-07-25 22:53:21 -0600

    add test

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/EnrollmentAgreementsCoCoPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/EnrollmentAgreementsCoCoPageSteps.java
@@ -3,6 +3,7 @@ package com.c4hco.test.automation.stepDefinitions.cocoSteps;
 import com.c4hco.test.automation.pages.cocoPages.EnrollmentAgreementsCoCoPage;
 import com.c4hco.test.automation.utils.WebDriverManager;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 
 public class EnrollmentAgreementsCoCoPageSteps {
     EnrollmentAgreementsCoCoPage enrollmentAgreementsCoCoPage = new EnrollmentAgreementsCoCoPage(WebDriverManager.getDriver());
@@ -18,5 +19,10 @@ public class EnrollmentAgreementsCoCoPageSteps {
     @And("I select continue button on the Enrollment Agreements CoCo page")
     public void continueInitialPaymentCoCo(){
         enrollmentAgreementsCoCoPage.selectContinueCoCo();
+    }
+
+    @Then("I validate the page text on the enrollment agreements page coco")
+    public void validatePageText(){
+        enrollmentAgreementsCoCoPage.validatePageText();
     }
 }
