95f817ce31d75d5f14c8c65dc13e3578434e5966
ypineda01 <ypineda@c4hco.com>
2024-08-02 12:50:55 -0600

    Updated functions and new tests @SLCR-183 SLER-792 WIP
    Updated the save function on my profile page for coco
    Left it commented out until we get the go for staging
    
    Did not run test in staging as this is not in staging only in QA updated function worked
    
    new tests saveChangesPopup.feature SLCR-183 and SLER-792 both WIP the tests will cover the functionality of the password popup after save button has been clicked

--- src/main/java/com/c4hco/test/automation/pages/cocoPages/MyProfilePage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoPages/MyProfilePage.java
@@ -1,5 +1,6 @@
 package com.c4hco.test.automation.pages.cocoPages;
 
+import com.c4hco.test.automation.Dto.SharedData;
 import com.c4hco.test.automation.utils.BasicActions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,11 +10,30 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 public class MyProfilePage {
+
+
+    @FindBy(id = "preferredLanguage")
+    WebElement  PreferredLanguageDrpCoCo;
+
+    @FindBy(css = "#preferredLanguage option")
+    List<WebElement> LanguageDrpOptionsCoCo; //// Select option, English, Spanish
+
+    @FindBy(id = "preferredContactMethod")
+    WebElement   PreferredContactDrpCoCo;
+
+    @FindBy(css = "#preferredContactMethod option")
+    List<WebElement>  PreferredContactDrpOptionsCoCo; // Select option, Email, Mail
     @FindBy(xpath = "//button[contains(@class,'back-to-welcome')]")
     public WebElement backToWelcomeButton;
 
     @FindBy(css = ".bottom_buttons_container button")
     List<WebElement> MyProfileButtonCoCo; /// Go back, Make changes, Save buttons
+
+    @FindBy(css = "#userPassword")
+    WebElement PasswordInputCoCo;
+
+    @FindBy(css = ".password-button")
+    WebElement PasswordSaveChangesCoCo;
 
     private BasicActions basicActions;
     public MyProfilePage(WebDriver webDriver) {
@@ -33,7 +53,46 @@ public class MyProfilePage {
     public void clickSaveButtonCoCo() {
         basicActions.waitForElementListToBePresent(MyProfileButtonCoCo, 40);
         MyProfileButtonCoCo.get(1).click();
+//        basicActions.waitForElementToBePresent(PasswordInputCoCo,40);
+//        PasswordInputCoCo.sendKeys(SharedData.getPrimaryMember().getPassword()); //QA feature branch
+//        basicActions.waitForElementToBePresent(PasswordSaveChangesCoCo,40);
+//        PasswordSaveChangesCoCo.click();
 
+    }
+
+
+    public void clickPreferredLanguageDrpCoCo() {
+        basicActions.waitForElementToBePresent(PreferredLanguageDrpCoCo, 60);
+        PreferredLanguageDrpCoCo.click();
+    }
+
+
+    public void clickPreferredContactDrpCoCo() {
+        basicActions.waitForElementToBePresent(PreferredContactDrpCoCo, 60);
+        PreferredContactDrpCoCo.click();
+    }
+
+
+    public void PreferredContactMethod(String contactPrefrences) {
+        clickPreferredContactDrpCoCo();
+        switch (contactPrefrences){
+            case "Email":
+                PreferredContactDrpOptionsCoCo.get(1).click();
+                break;
+            case "Mail":
+                PreferredContactDrpOptionsCoCo.get(2).click();
+        }
+    }
+
+    public void PreferredContactLanguageMethod(String langContactPreferred) {
+        clickPreferredLanguageDrpCoCo();
+        switch (langContactPreferred){
+            case "English":
+                LanguageDrpOptionsCoCo.get(1).click();
+                break;
+            case "Spanish":
+                LanguageDrpOptionsCoCo.get(2).click();
+        }
     }
 
 }
---
+++ src/test/resources/features/COCO/NVO/My Profile Page/saveChangesPopup.feature
@@ -0,0 +1,18 @@
+Feature: My Profile Page Coco
+
+  Background: I go the login portal
+    Given I open the login page on the "login" portal
+
+  @SLCR-183 @WIP
+  Scenario: Save Changes Popup on My Profile page
+    When I click create a new account on login page
+    Then I click create my account from pre-screen page
+    And I enter general mandatory data for "coco" account creation
+    Then I validate I am on the "Login" page
+    And  I enter valid credentials to login
+    Then I validate I am on the "CoCo Welcome" page
+    And I click on "My Profile" link on welcome page
+    Then I validate I am on the "My Profile" page
+    And I click on Make Changes Button in CoCo
+    And I change Preferred Language Contact Method to "Spanish"
+    And I click on Save Button on My profile CoCo
