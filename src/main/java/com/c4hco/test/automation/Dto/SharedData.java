9d372c00e4323a04af2f3f3f11f42229c0db2240
URose12 <urose@c4hco.com>
2024-08-16 11:17:39 -0600

    Changes to Scenario details

--- src/main/java/com/c4hco/test/automation/Dto/SharedData.java
+++ src/main/java/com/c4hco/test/automation/Dto/SharedData.java
@@ -32,7 +32,6 @@ public class SharedData {
     @Getter @Setter private static List<PlanDbData> dentalPlanDbData;
     @Getter @Setter private static String planYear;
     @Getter @Setter private static GroupDetails groupDetails;
-    @Getter @Setter private static int totalGroups;
     @Getter @Setter private static String dbName;
     @Getter @Setter private static String isOpenEnrollment;
     @Getter @Setter private static Map<String, String> memIdAndMedicalFileName;
@@ -44,9 +43,8 @@ public class SharedData {
     @Getter @Setter private static String noticeFileName;
     @Getter @Setter private static List<BookOfBusinessQEntity> bookOfBusinessQEntityList;
     @Getter @Setter private static Map<String, List<String>> groupingDetails;
-    @Getter @Setter private static String subscribers;
-    @Getter @Setter private static String dependents;
-    @Getter @Setter private static String enrollees;
+    @Getter @Setter private static ScenarioDetails scenarioDetails;
+
 
     public static void reset(){
         firstPlanNameOnMedicalResultsPage = null;
@@ -60,14 +58,13 @@ public class SharedData {
         env = null;
         ob834DetailsEntities = null;
         dbData = null;
-        totalGroups = 0;
         planYear = null;
         groupDetails = null;
         dbName = null;
         noticeFileName = null;
         bookOfBusinessQEntityList = null;
         isOpenEnrollment = null;
-
+        scenarioDetails = null;
     }
 
 
--- src/main/java/com/c4hco/test/automation/pages/exchPages/AccountOverviewPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/AccountOverviewPage.java
@@ -1,6 +1,7 @@
 package com.c4hco.test.automation.pages.exchPages;
 
 import com.c4hco.test.automation.Dto.MemberDetails;
+import com.c4hco.test.automation.Dto.ScenarioDetails;
 import com.c4hco.test.automation.Dto.SharedData;
 import com.c4hco.test.automation.utils.BasicActions;
 import org.openqa.selenium.WebDriver;
@@ -164,13 +165,18 @@ public class AccountOverviewPage {
 
     public void setScenarioDetails(List<Map<String, String>> expectedResult) {
         String noOfGroups = expectedResult.get(0).get("totalGroups");
+        String totalMembers = expectedResult.get(0).get("totalMembers");
         String totalSubscribers = expectedResult.get(0).get("total_subscribers");
         String totalDependents = expectedResult.get(0).get("total_dependents");
         String totalEnrollees = expectedResult.get(0).get("total_enrollees");
-        SharedData.setTotalGroups(Integer.parseInt(noOfGroups));
-        SharedData.setSubscribers(totalSubscribers);
-        SharedData.setDependents(totalDependents);
-        SharedData.setEnrollees(totalEnrollees);
-    }
+        ScenarioDetails scenarioDetails = new ScenarioDetails();
+        scenarioDetails.setTotalGroups(Integer.parseInt(noOfGroups));
+        scenarioDetails.setTotalMembers(Integer.parseInt(totalMembers));
+        scenarioDetails.setSubscribers(totalSubscribers);
+        scenarioDetails.setDependents(totalDependents);
+        scenarioDetails.setEnrollees(totalEnrollees);
+        SharedData.setScenarioDetails(scenarioDetails);
+
+   }
 
 }
