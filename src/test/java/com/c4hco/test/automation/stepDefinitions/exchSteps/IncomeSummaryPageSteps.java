41eaaf614927f1edf2e20ee9d63c5b7553befe1c
marcigreene <77079569+marcigreene@users.noreply.github.com>
2024-08-21 15:18:04 -0600

    SLER-956, SLER-957 Complete
    •	Added tests for help drawer
    o	EXCH\TAM\Income Summary page\helpDrawer.feature
    •	Updated steps for new tests
    o	exchPages\IncomeSummaryPage
    o	exchSteps\IncomeSummaryPageSteps

--- src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/IncomeSummaryPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/IncomeSummaryPageSteps.java
@@ -13,18 +13,29 @@ public class IncomeSummaryPageSteps {
         incomeSummaryPage.selectProjectedIncome(projectedIncomeOption);
 
     }
+
     @Then("I click the save and continue button on the Income Summary Detail page")
-    public void iClickSaveandContinue(){
+    public void iClickSaveandContinue() {
         incomeSummaryPage.clickSaveandContinue();
     }
 
+    @And("I select {string} icon on the Income Summary Page")
+    public void iSelectHelpIcon(String labelIcon) {incomeSummaryPage.clickHelpIcon(labelIcon);}
 
-
-
+    @Then("I click on Contact Us link in Help Drawer on the Income Summary page")
+    public void iClickHelpDrawerContactUs(){incomeSummaryPage.clickHelpContactUsNavigation();}
 
 
 
     // ############################## VALIDATION STEPS #########################
     // Add only validation methods below this line
     @And("I verify the header for Primary Member on the Income Summary page in {string}")
-    public void iVerifyTextOnIncomeSummaryPage(String language){incomeSummaryPage.verifyHeadersIncomeSummaryPage(language);}}
+    public void iVerifyTextOnIncomeSummaryPage(String language) {
+        incomeSummaryPage.verifyHeadersIncomeSummaryPage(language);
+    }
+
+    @Then("I validate Income Summary help drawer {string} verbiage in {string}")
+    public void iValidateHelp(String helpText, String language) {incomeSummaryPage.validateHelpVerbiage(helpText, language);}
+
+
+}
