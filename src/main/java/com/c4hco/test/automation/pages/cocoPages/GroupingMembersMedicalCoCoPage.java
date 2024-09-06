9d18cc91f5ce2099eb68c8e15b44dfd1d8ec1dfa
troyhenning <94495591+troyhenning@users.noreply.github.com>
2024-09-06 13:21:50 -0600

    Update GroupingMembersMedicalCoCoPage.java
    removed Angular timer and added time to spinner element

--- src/main/java/com/c4hco/test/automation/pages/cocoPages/GroupingMembersMedicalCoCoPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoPages/GroupingMembersMedicalCoCoPage.java
@@ -53,8 +53,7 @@ public class GroupingMembersMedicalCoCoPage {
     }
 
     public void clickContinue() {
-        basicActions.waitForElementToDisappear( spinner,15 );
-        basicActions.waitForAngular( 20 );
+        basicActions.waitForElementToDisappear( spinner,25 );
         basicActions.waitForElementToBePresent(continueButton, 20);
         basicActions.waitForElementToBeClickable(continueButton, 30);
         continueButton.click();
