ca182bf1fd3be8ddba72a2484cc3220b599cbce4
troyhenning <94495591+troyhenning@users.noreply.github.com>
2024-09-25 16:04:08 -0600

    Update MedicalPlansCoCoPageSteps.java

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/MedicalPlansCoCoPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoSteps/MedicalPlansCoCoPageSteps.java
@@ -62,7 +62,7 @@ public class MedicalPlansCoCoPageSteps {
     @And("I validate the COCO plan option {int} has text {string}")
     public void validatePlanText(int Index, String planText) {
         medicalPlansCoCoPage.validatePlanResults(Index, planText);}
-    
+
     @Then("I validate sorting based on {string} in coco medical page")
     public void validateMedicalPremiumSorting(String sortingTypeSelection) {
         medicalPlansCoCoPage.evaluateSortingValue(sortingTypeSelection);
