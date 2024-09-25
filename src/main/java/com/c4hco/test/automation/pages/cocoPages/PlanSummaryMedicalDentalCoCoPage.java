9a4ab24a40bd76c89c9b6ad4721d64e54642fd7d
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2024-09-25 18:59:50 +0530

    Updated script for SLCR-328 and SLCR-329
    Updated script for SLCR-328 and SLCR-329

--- src/main/java/com/c4hco/test/automation/pages/cocoPages/PlanSummaryMedicalDentalCoCoPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoPages/PlanSummaryMedicalDentalCoCoPage.java
@@ -8,9 +8,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
+import java.util.List;
+
 public class PlanSummaryMedicalDentalCoCoPage {
 
     private BasicActions basicActions;
+    SoftAssert softAssert = new SoftAssert();
 
     public PlanSummaryMedicalDentalCoCoPage(WebDriver webDriver) {
         basicActions = new BasicActions(webDriver);
@@ -29,6 +32,42 @@ public class PlanSummaryMedicalDentalCoCoPage {
     @FindBy(id = "PlanSummary-GoBack")
     WebElement goBackbtn;
 
+    @FindBy(xpath = "//a[@id='PlanSummary-medicalDentalPDF']/em")
+    WebElement txtmedicalDentalPDF;
+
+    @FindBy(xpath = "//div[@class='summary-container'] //p[@class='title-text']")
+    List<WebElement> txttitle;
+
+    @FindBy(css = ".th-title.header-2")
+    WebElement txtPlanHeader;
+
+    @FindBy(id = "PlanSummary-MedicalPlanChangePlan_0")
+    WebElement txtMedicalPlanChange;
+
+    @FindBy(css = ".float-start.link-text-2")
+    WebElement txtViewPlanDetails;
+
+    @FindBy(xpath = "//span[@class='plan-amount']/following-sibling::span")
+    WebElement txtPlanamnt;
+
+    @FindBy(xpath = "//span[@id='PlanSummary-TotalAmountYouPay']/following-sibling::span")
+    WebElement txtTotalAmountYouPay;
+
+    @FindBy(xpath ="(//div[@class='plan-card plan-summary-card']/div)[1]")
+    WebElement txtTotalofMedicalPlan;
+
+    @FindBy(xpath ="((//div[@class='plan-card plan-summary-card']/div)[2] //span)[1]")
+    WebElement txtPremiumsBeforeSaving;
+
+    @FindBy(xpath ="((//div[@class='plan-card plan-summary-card']/div)[3] //span)[1]")
+    WebElement txtMoneyYouSave;
+
+    @FindBy(id = "PlanSummary-SaveAndExit")
+    WebElement SvaeandExitbtn;
+
+    @FindBy(xpath ="((//div[@class='plan-card plan-summary-card']/div)[4] //span)[1]")
+    WebElement txtAmountyouPay;
+
     public void continueButton(){
         basicActions.waitForElementToDisappear(spinner, 20);
         basicActions.waitForElementToBePresent(medicalPremiumAfterAPTCAmt, 10);
@@ -42,5 +81,23 @@ public class PlanSummaryMedicalDentalCoCoPage {
         basicActions.waitForElementToBePresent(goBackbtn,10);
         basicActions.scrollToElement(goBackbtn);
         goBackbtn.click();
+    }
+
+    public void verifyTextinPlanSummary(List<String> textDetails){
+        basicActions.waitForElementToDisappear(spinner,20);
+        softAssert.assertEquals(txtmedicalDentalPDF.getText(),textDetails.get(0));
+        softAssert.assertEquals(txttitle.get(0).getText(),textDetails.get(1));
+        softAssert.assertEquals(txttitle.get(1).getText(),textDetails.get(2));
+        softAssert.assertEquals(txtPlanHeader.getText(),textDetails.get(3));
+        softAssert.assertEquals(txtMedicalPlanChange.getText(),textDetails.get(4));
+        softAssert.assertEquals(txtPlanamnt.getText(),textDetails.get(5));
+        softAssert.assertEquals(txtTotalAmountYouPay.getText(),textDetails.get(5));
+        softAssert.assertEquals(txtTotalofMedicalPlan.getText(),textDetails.get(6));
+        softAssert.assertEquals(txtPremiumsBeforeSaving.getText(),textDetails.get(7));
+        softAssert.assertEquals(txtMoneyYouSave.getText(),textDetails.get(8));
+        softAssert.assertEquals(txtAmountyouPay.getText(),textDetails.get(9));
+        softAssert.assertEquals(goBackbtn.getText(),textDetails.get(10));
+        softAssert.assertEquals(SvaeandExitbtn.getText(),textDetails.get(11));
+        softAssert.assertEquals(continueBtnOnPlanSummary.getText(),textDetails.get(12));
     }
 }
