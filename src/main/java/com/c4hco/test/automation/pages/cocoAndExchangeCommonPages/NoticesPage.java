d243d1482f0b1721eed648c5862c5e6de1c3c7d8
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-09-27 18:22:43 -0600

    add aptc validations on accc overview page

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/NoticesPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/NoticesPage.java
@@ -477,6 +477,7 @@ public class NoticesPage {
         softAssert.assertAll();
     }
 
+
     private void validateMembers(String locatorStringByPlan, List<String> membersOnPolicy){
         for (String memPrefix : membersOnPolicy) {
             String memberName = getMemFullName(memPrefix);
