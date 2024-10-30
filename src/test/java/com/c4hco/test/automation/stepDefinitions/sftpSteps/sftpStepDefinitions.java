3ab4826748b5823f8866589338287915a0e1e30a
URose12 <urose@c4hco.com>
2024-10-30 11:08:28 -0600

    Update to sftpStepDefinitions file

--- src/test/java/com/c4hco/test/automation/stepDefinitions/sftpSteps/sftpStepDefinitions.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/sftpSteps/sftpStepDefinitions.java
@@ -183,9 +183,4 @@ public class sftpStepDefinitions {
         sftpUtil.readIb999File(SharedData.getMedicalFileName());
     }
 
-    @And("I download medical Ib834 file to local from sftp server location {string}")
-    public void downloadIb834FileToTarget(String remoteLocation) throws JSchException {
-        sftpUtil.downloadFileWithSftp(remoteLocation, SharedData.getMedicalIb834FileName());
-            }
-
 }
