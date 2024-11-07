be3a394ebf7feb7f03db49c8f9475a8bb9ba849c
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2024-11-07 17:56:31 +0530

    Updated Script
    Updated Script

--- src/main/java/com/c4hco/test/automation/pages/exchPages/GroupingMembersMedicalPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/GroupingMembersMedicalPage.java
@@ -63,11 +63,6 @@ public class GroupingMembersMedicalPage {
             memDet.setMedGroupInd(memGroupInfo.getText().replace("Medical Group #",""));
         }
         basicActions.scrollToElement( continueButton );
-        List<MemberDetails> memberInfoDetails = basicActions.getAllMem();
-        for (MemberDetails memDet : memberInfoDetails){
-            WebElement memGroupInfo = basicActions.getDriver().findElement(By.xpath("//span[contains(text(),'"+memDet.getFirstName()+"')]/ancestor-or-self::div[@class='group-member__container']/div[@class='c4-type-header-sm group-member__Header']"));
-            memDet.setMedGroupInd(memGroupInfo.getText().replace("Medical Group #",""));
-        }
         basicActions.click(continueButton);
     }
 
