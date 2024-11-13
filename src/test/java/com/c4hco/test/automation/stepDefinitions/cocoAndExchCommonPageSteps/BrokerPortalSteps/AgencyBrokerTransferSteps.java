47801dee7f8bb5a5c3122041bfecbfc31e1924b9
carrimartin <91921791+carrimartin@users.noreply.github.com>
2024-11-13 10:29:09 -0700

    Crated SLER-1263, SLER-1264, SLER-1265 & SLER-1266

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/BrokerPortalSteps/AgencyBrokerTransferSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/BrokerPortalSteps/AgencyBrokerTransferSteps.java
@@ -13,14 +13,34 @@ public class AgencyBrokerTransferSteps {
         agencyBrokerTransferPage.searchBrokerOnAgencyBrokerTransfer(brokerName);
     }
 
+    @Then("I search for a broker by license number {string} on the Agency Broker Transfer page")
+    public void iSearchForBrokerLicenseOnAgencyBrokerTransfer(String brokerLicense){
+        agencyBrokerTransferPage.searchBrokerLicenseOnAgencyBrokerTransfer(brokerLicense);
+    }
+
+    @And("I clear the broker search box on the Agency Broker Transfer page")
+    public void iClearBrokerSearchBoxAgencyBrokerTransfer(){
+        agencyBrokerTransferPage.clearBrokerSearchBoxAgencyBrokerTransfer();
+    }
+
     @And("I validate the broker results match my broker named {string}")
     public void iValidateBrokerSearchResults(String brokerName){
         agencyBrokerTransferPage.validateBrokerSearchResults(brokerName);
     }
 
+    @And("I validate the new broker details are {string}, {string}, or {string}, {string}, {string}, {string}, {string}")
+    public void iValidateBrokerSearchResultsDetails(String brokerName, String brokerEmailStg, String brokerEmailQa, String brokerPhone, String contactType, String agencyName, String brokerLicense){
+        agencyBrokerTransferPage.validateBrokerSearchResultsDetails(brokerName, brokerEmailStg, brokerEmailQa, brokerPhone, contactType, agencyName, brokerLicense);
+    }
+
     @Then("I click the transfer link on the Agency Broker Transfer page")
     public void iClickTransferLink(){
         agencyBrokerTransferPage.clickTransferLink();
+    }
+
+    @Then("I click the go back button on the Agency Broker Transfer page")
+    public void iClickTransferGoBackButton(){
+        agencyBrokerTransferPage.clickTransferGoBackButton();
     }
 
     @Then("I validate the Agency Broker Transfer page title")
