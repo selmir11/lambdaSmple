d687c84abe18382dab1c0ad4aec1ea5c0dcad071
carrimartin <91921791+carrimartin@users.noreply.github.com>
2024-12-06 08:34:46 -0700

    Converted RT-1862 to SLER-1817 & SLER-1818

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/BrokerPortalPages/ReviewTransferPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/BrokerPortalPages/ReviewTransferPage.java
@@ -9,8 +9,41 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
 public class ReviewTransferPage {
+    @FindBy(xpath = "//*[@id='elem']/app-review-transfer/div/div[1]")
+    WebElement reviewTransferPageTitle;
+
+    @FindBy(id = "transfer-brokerName-label")
+    WebElement newBrokerFieldLabel;
+
+    @FindBy(id = "transfer-brokerName")
+    WebElement newBrokerField;
+
+    @FindBy(id = "transfer-agencyName-label")
+    WebElement newAgencyNameFieldLabel;
+
+    @FindBy(id = "transfer-agencyName")
+    WebElement newAgencyNameField;
+
+    @FindBy(id = "transfer-brokerAddress-label")
+    WebElement newBrokerAddressFieldLabel;
+
+    @FindBy(id = "transfer-brokerAddress")
+    WebElement newBrokerAddressField;
+
+    @FindBy(id = "transfer-brokerEmail-label")
+    WebElement newBrokerEmailFieldLabel;
+
+    @FindBy(id = "transfer-brokerEmail")
+    WebElement newBrokerEmailField;
+
     @FindBy(id = "transfer-fullName")
     WebElement transferResultsClientName;
+
+    @FindBy(id = "transfer-confirm-msg")
+    WebElement transferConfirmationText;
+
+    @FindBy(xpath = "//*[@id='elem']/app-review-transfer/div/div[2]/div[4]")
+    WebElement transferWarningText;
 
     @FindBy(xpath = "//button[normalize-space()='Transfer Clients']")
     WebElement transferClientsButton;
@@ -42,6 +75,53 @@ public class ReviewTransferPage {
     public ReviewTransferPage(WebDriver webDriver){
         basicActions = new BasicActions(webDriver);
         PageFactory.initElements(basicActions.getDriver(), this);
+    }
+
+    public void validateReviewTransferPageTitle(){
+        basicActions.waitForElementToBePresent(reviewTransferPageTitle,10);
+        softAssert.assertEquals(reviewTransferPageTitle.getText(), "Review Transfer");
+        softAssert.assertAll();
+    }
+
+    public void validateNewBrokerFieldLabels(){
+        basicActions.waitForElementToBePresent(newBrokerFieldLabel,10);
+        softAssert.assertEquals(newBrokerFieldLabel.getText(), "Broker Name:");
+        softAssert.assertEquals(newAgencyNameFieldLabel.getText(), "Agency Name:");
+        softAssert.assertEquals(newBrokerAddressFieldLabel.getText(), "Agency Address:");
+        softAssert.assertEquals(newBrokerEmailFieldLabel.getText(), "Email Address:");
+        softAssert.assertAll();
+    }
+
+    public void validateNewBrokerTransferDetails(String brokerName, String agencyName, String brokerAddress, String brokerEmailSTG, String brokerEmailQA){
+        basicActions.waitForElementToBePresent(newBrokerField,10);
+        softAssert.assertEquals(newBrokerField.getText(), brokerName);
+        softAssert.assertEquals(newAgencyNameField.getText(), agencyName);
+        softAssert.assertEquals(newBrokerAddressField.getText(), brokerAddress);
+        if(SharedData.getEnv().equals("staging")){
+            softAssert.assertEquals(newBrokerEmailField.getText(), brokerEmailSTG);
+        } else{
+            softAssert.assertEquals(newBrokerEmailField.getText(), brokerEmailQA);
+        }
+        softAssert.assertAll();
+    }
+
+    public void validateTransferConfirmationText(String brokerName, String agencyName){
+        basicActions.waitForElementToBePresent(transferConfirmationText,10);
+        softAssert.assertEquals(transferConfirmationText.getText(), "The following client(s) will be transferred to " + brokerName + " within " + agencyName);
+        softAssert.assertAll();
+    }
+
+    public void validateReviewTransferButtons(){
+        basicActions.waitForElementToBePresent(transferClientsButton,10);
+        softAssert.assertEquals(transferClientsButton.getText(), "Transfer Clients");
+        softAssert.assertEquals(transferClientsCancelButton.getText(), "Cancel");
+        softAssert.assertAll();
+    }
+
+    public void validateTransferWarningText(){
+        basicActions.waitForElementToBePresent(transferWarningText,10);
+        softAssert.assertEquals(transferWarningText.getText(), "By Selecting \"Transfer Clients\" below, the client(s) will no longer appear in your client list and you will not be able to work on their behalf.");
+        softAssert.assertAll();
     }
 
     public void validateClientNameDisplayed(){
