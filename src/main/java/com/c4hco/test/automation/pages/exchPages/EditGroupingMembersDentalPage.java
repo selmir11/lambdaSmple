a37a93c9dd36716bad1b10425be15ac09d004acc
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2024-12-06 20:36:50 +0530

    Updated Script
    Updated Script

--- src/main/java/com/c4hco/test/automation/pages/exchPages/EditGroupingMembersDentalPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/EditGroupingMembersDentalPage.java
@@ -115,39 +115,6 @@ public class EditGroupingMembersDentalPage {
         }
     }
 
-    public void createNewDentalGroups2(List<String> grouping) {
-        basicActions.waitForElementToDisappear(spinner, 70);
-        basicActions.waitForElementToBePresent(createNewGroupLink, 70);
-        basicActions.scrollToElement(createNewGroupLink);
-
-        while (dragAMemberHere.size()<grouping.size()) {
-            basicActions.scrollToElement(createNewGroupLink);
-            createNewGroupLink.click();
-        }
-
-        for (int i = grouping.size() - 1; i >= 0; i--) {
-            String[] groupDetail = grouping.get(i).split(":");
-            String[] Names = groupDetail[0].split(",");
-
-            for (String Name : Names) {
-                WebElement dragElement = basicActions.getDriver()
-                        .findElement(By.xpath("//*[@id='SOL-ManageGroupingMembers-MemberDetails']/span[contains(text(), '" + Name + "')]"));
-
-                WebElement dropElement = dragAMemberHere.get(i);
-
-                basicActions.scrollToElement(dragElement);
-                basicActions.scrollToElement(dropElement);
-
-                builder.clickAndHold(dragElement)
-                        .moveToElement(dropElement)
-                        .release(dropElement).build()
-                        .perform();
-
-                basicActions.wait(3000);
-            }
-        }
-    }
-
     public void iClickSaveButton(){
         softAssert.assertTrue(saveButtonOnEditDentalGroupingPage.isEnabled());
         basicActions.waitForElementToBePresent(saveButtonOnEditDentalGroupingPage,20);
