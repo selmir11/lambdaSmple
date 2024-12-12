a378516b3f636badf5ce3fcd7682db8ba16aa632
nivikC4h <131700079+nivikC4h@users.noreply.github.com>
2024-12-12 12:14:35 -0700

    tobacco usage code

--- src/main/java/com/c4hco/test/automation/pages/exchPages/StartShoppingPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/StartShoppingPage.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
+import java.util.Map;
 
 public class StartShoppingPage {
     private BasicActions basicActions;
@@ -78,56 +80,34 @@ public class StartShoppingPage {
         basicActions.scrollToElement( goBackbtn );
         goBackbtn.click();
     }
-
-    public void enterTobaccoUsageInfo(String tobaccoUsageDetails) {
-        basicActions.waitForElementToDisappear( spinner, 20 );
-        basicActions.waitForElementListToBePresent( btnNoAndYes, 10 );
-
-        String[] nameDetails = tobaccoUsageDetails.split( "," );
-
-        for (String name : nameDetails) {
-            WebElement tobaccoUsageYes = basicActions.getDriver().findElement( By.xpath( "(//Strong[contains(text(),'" + name + "')]/parent::p/following-sibling::label)[1]" ) );
-            tobaccoUsageYes.click();
-            setTobaccoUser( name );
-        }
-    }
-
-    private void setTobaccoUser(String name) {
-        if (name.contains( "primary" )) {
-            SharedData.getPrimaryMember().setTobacco_user( "Yes" );
-        } else {
-            List<MemberDetails> dependents = SharedData.getMembers();
-            for (MemberDetails mem : dependents) {
-                if (mem.getFirstName().contains( name )) {
-                    mem.setTobacco_user( "Yes" );
-                }
-            }
-        }
-    }
-    public void isTobaccoUser(String tobaccoUsageYesOrNo, String tobaccoUsageDetails) {
+    public void selectTobaccoUsage(String tobaccoUsageYesOrNo, String tobaccoUsageDetails) {
         basicActions.waitForElementToDisappear(spinner, 20);
         basicActions.waitForElementListToBePresent(btnNoAndYes, 10);
         String[] nameDetails = tobaccoUsageDetails.split(",");
-        for (String name : nameDetails)
-        {
-            List<MemberDetails> members = basicActions.getAllMem();
-            for (MemberDetails mem : members)
-            {
-                switch (tobaccoUsageYesOrNo) {
-                    case "Yes":
-                        WebElement tobaccoUsageYes = basicActions.getDriver().findElement(By.xpath("(//*[contains(text(),'" + name + "')]/parent::p/following-sibling::label)[1]"));
-                        tobaccoUsageYes.click();
-                        if (mem.getFirstName().contains( name ))
-                            mem.setTobacco_user( "Yes" );
-                        break;
-                    case "No":
-                        WebElement tobaccoUsageNo = basicActions.getDriver().findElement(By.xpath("(//*[contains(text(),'" + name + "')]/parent::p/following-sibling::label)[2]"));
-                        tobaccoUsageNo.click();
-                        if (mem.getFirstName().contains( name ))
-                            mem.setTobacco_user( "No" );
-                }
-            }
+        for (String name : nameDetails) {
+            clickTobaccoUsage(tobaccoUsageYesOrNo, name);
         }
+    }
+
+    private void clickTobaccoUsage(String tobaccoUsageYesOrNo, String namePrefix){
+        switch (tobaccoUsageYesOrNo) {
+            case "Yes":
+                WebElement tobaccoUsageYes = basicActions.getDriver().findElement(By.xpath("(//*[contains(text(),'" + namePrefix + "')]/parent::p/following-sibling::label)[1]"));
+                tobaccoUsageYes.click();
+                break;
+            case "No":
+                WebElement tobaccoUsageNo = basicActions.getDriver().findElement(By.xpath("(//*[contains(text(),'" + namePrefix + "')]/parent::p/following-sibling::label)[2]"));
+                tobaccoUsageNo.click();
+                break;
+            default:
+                Assert.fail("Invalid argument passed!!");
+        }
+        setTobaccoUsage(tobaccoUsageYesOrNo, namePrefix);
+    }
+
+    private void setTobaccoUsage(String tobaccoUsageYesOrNo, String namePrefix){
+        List<MemberDetails> allMemList = basicActions.getAllMem();
+        allMemList.stream().filter(mem -> mem.getFirstName().contains(namePrefix)).findFirst().ifPresent(mem -> mem.setTobacco_user(tobaccoUsageYesOrNo));
     }
 
 }
