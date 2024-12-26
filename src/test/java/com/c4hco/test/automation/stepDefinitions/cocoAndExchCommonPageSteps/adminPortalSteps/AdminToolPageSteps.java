fd53afe3670177574223f7610c6152b3ef397a20
GauriJog <gjog@c4hco.com>
2024-12-26 16:21:39 -0700

    Completed SLER - 1906
    1) This is new test for new admin lce
    2) For now this is only available in specific url and in QA only (just for UI- Services are not yet connected.).

--- src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/adminPortalSteps/AdminToolPageSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/cocoAndExchCommonPageSteps/adminPortalSteps/AdminToolPageSteps.java
@@ -35,4 +35,45 @@ public class AdminToolPageSteps {
     public void iInitiateIncomingPage() {
         adminLceToolPage.initiateIncomingPage();
     }
+
+    // new admin LCE page steps - Expected to release in QA by mid-Feb
+    @Then("I navigate to admin lce url")
+    public void iNavigateToAdminLceUrl() {
+        adminLceToolPage.navigatetoLCE();
+    }
+
+    @Then("I verify Title for admin LCE page")
+    public void iVerifyTitleForAdminLCEPage() {
+      adminLceToolPage.verifyAdminLceTitle();
+    }
+
+    @Then("I enter an account ID in {string} {string} accountID field")
+    public void iEnterAnAccountIDInAccountIDField(String accountIdSTG, String accountIdQA) {
+        adminLceToolPage.enterAccountIdToAnyENV(accountIdSTG,accountIdQA);
+    }
+
+    @Then("I click on Look up button")
+    public void iClickOnLookUpButton() {
+        adminLceToolPage.clickLookUpButton();
+    }
+
+    @Then("I click on verify select plan year to edit message is displaying on admin lce page")
+    public void iClickOnVerifySelectPlanYearToEditMessageIsDisplayingOnAdminLcePage() {
+        adminLceToolPage.validateSelectPlanYearMessage();
+    }
+
+    @Then("I verify current year and previous three financial years are displaying in applictaion data")
+    public void iVerifyCurrentYearAndPreviousFinancialYearsAreDisplayingInApplictaionData() {
+        adminLceToolPage.verifyapplicationyearsData();
+    }
+
+    @Then("I click on application data dropdown arrow")
+    public void iClickOnApplicationDataDropdownArrow() {
+       adminLceToolPage.clickAppDataDropdown();
+    }
+
+    @Then("I verify no application data message is displayed")
+    public void iVerifyNoApplicationDataMessageIsDisplayed() {
+        adminLceToolPage.noappdatamessage();
+    }
 }
