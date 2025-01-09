e1e9155516f04a368801c83986cb3e21614f6540
sharmaraj-s <102505829+sharmaraj-s@users.noreply.github.com>
2025-01-09 14:06:35 +0530

    Update AccountOverviewPage.java

--- src/main/java/com/c4hco/test/automation/pages/exchPages/AccountOverviewPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/AccountOverviewPage.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-import org.openqa.selenium.By;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
