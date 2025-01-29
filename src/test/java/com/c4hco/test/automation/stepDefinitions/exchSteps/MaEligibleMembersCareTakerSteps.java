b1c0723608255bc3153fe90ebba70c67d49d62d4
troyhenning <94495591+troyhenning@users.noreply.github.com>
2025-01-29 14:59:54 -0700

    update

--- src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/MaEligibleMembersCareTakerSteps.java
+++ src/test/java/com/c4hco/test/automation/stepDefinitions/exchSteps/MaEligibleMembersCareTakerSteps.java
@@ -11,4 +11,8 @@ public class MaEligibleMembersCareTakerSteps {
     @Then("I select {string} for under the age of 19 question and Continue on ParentCaretaker MA page")
     public void iAnswerUnder19(String yesNo) {maEligibleMembersCareTakerForMAPage.answerUnder19(yesNo);}
 
+    @Then( "I validate the {String} text on the Caretaker page" )
+    public void textCaretaker(String language) {maEligibleMembersCareTakerForMAPage.textCaretaker(language);}
+
+
 }
