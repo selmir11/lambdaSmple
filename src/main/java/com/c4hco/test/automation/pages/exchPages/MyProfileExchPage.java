808ce35a0cd9b3662a1e3043b234c1ab8c80691a
TeresaSpelling <77080459+TeresaSpelling@users.noreply.github.com>
2025-02-17 11:47:20 -0600

    RT-2053(sler2184) & RT-2531(sler2182) & RT-2539(sler2191)

--- src/main/java/com/c4hco/test/automation/pages/exchPages/MyProfileExchPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/MyProfileExchPage.java
@@ -532,6 +532,15 @@ public class MyProfileExchPage {
         savePrimaryContact.click();
         basicActions.wait(60);
     }
+    public void iChangePrimaryContact() {
+        basicActions.waitForElementToBeClickable(changePrimaryContact, 20);
+        changePrimaryContact.click();
+        basicActions.waitForElementToBeClickable(primaryContactDRP, 20);
+        primaryContactDRP.click();
+        dpdPrimaryChangeOpt2.click();
+        savePrimaryContact.click();
+        basicActions.wait(60);
+    }
 
     public void verifyPasswordPopupTextOnMyProfileExchPage(String language) {
         switch (language) {
