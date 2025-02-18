7f3394bb6c8f634423f3f0fecb93cb2fa379322c
selmir11 <selmir@c4hco.com>
2025-02-18 15:41:34 -0700

    update
    update

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/TaxToolsPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/TaxToolsPage.java
@@ -1,20 +1,14 @@
 package com.c4hco.test.automation.pages.cocoAndExchangeCommonPages;
 
 import com.c4hco.test.automation.utils.BasicActions;
-import io.cucumber.java.en.Then;
-import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.asserts.SoftAssert;
 
-import javax.swing.*;
-import java.util.List;
 
 public class TaxToolsPage {
     private BasicActions basicActions;
