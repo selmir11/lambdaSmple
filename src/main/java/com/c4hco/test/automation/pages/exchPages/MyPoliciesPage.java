b1236b76cfd5718a966365a7c3f8b0dd1fb9a79c
URose12 <urose@c4hco.com>
2025-03-04 13:20:13 -0700

    Fix to eapid validation

--- src/main/java/com/c4hco/test/automation/pages/exchPages/MyPoliciesPage.java
+++ src/main/java/com/c4hco/test/automation/pages/exchPages/MyPoliciesPage.java
@@ -118,7 +118,7 @@ public class MyPoliciesPage {
 
             Map<String, String> denEapidDb = exchDbDataProvider.getDentalEap_id();
             member.setDentalEapid_db(denEapidDb.get(member.getDenGroupInd()));
-            softAssert.assertEquals(denEapidDb.get(member.getDenGroupInd()),EAPID.getText().replace("Policy Number: ",""), "Medical EAP_ID from My Policies page does not match EAP_ID plan summary page");
+            softAssert.assertEquals(denEapidDb.get(member.getDenGroupInd()),EAPID.getText().replace("Policy number: ",""), "Medical EAP_ID from My Policies page does not match EAP_ID plan summary page");
             softAssert.assertEquals(lastUpdatedOn.getText(),expecLastUpdatedDate, "Last Updated On mismatch for member: " + member.getFirstName());
             softAssert.assertAll();
         }
@@ -159,7 +159,7 @@ public class MyPoliciesPage {
 
             Map<String, String> medEapidDb = exchDbDataProvider.getMedicalEap_id();
             member.setMedicalEapid_db(medEapidDb.get(member.getMedGroupInd()));
-            softAssert.assertEquals(medEapidDb.get(member.getMedGroupInd()),EAPID.getText().replace("Policy Number: ",""), "Medical EAP_ID from My Policies page does not match EAP_ID plan summary page");
+            softAssert.assertEquals(medEapidDb.get(member.getMedGroupInd()),EAPID.getText().replace("Policy number: ",""), "Medical EAP_ID from My Policies page does not match EAP_ID plan summary page");
             softAssert.assertEquals(lastUpdatedOn.getText(),expecLastUpdatedDate, "Last Updated On mismatch for member: " + member.getFirstName());
             softAssert.assertAll();
         }
@@ -212,9 +212,6 @@ public class MyPoliciesPage {
     
     public void validateCancelPlan(String planType){
          By locator = By.id("SOL-CurrentPolicies-CancelCurrent" + planType + "\")");
-         if(basicActions.elementExists(basicActions.getDriver(), locator)) {
-             List<WebElement> elements = basicActions.getDriver().findElements(locator);
-             Assert.assertTrue(elements.isEmpty(), "Element should be absent but was found.");
-         }
+         softAssert.assertFalse(basicActions.elementExists(locator),"Element should be absent but was found.");
     }
 }
