444f7d120e633ec9c7c55770310a0d82e440b512
SShephali <143009582+SShephali@users.noreply.github.com>
2025-04-22 20:57:05 +0530

    SLER-2459 WIP

--- src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/MyDocumentsPage.java
+++ src/main/java/com/c4hco/test/automation/pages/cocoAndExchangeCommonPages/MyDocumentsPage.java
@@ -532,7 +532,8 @@ public class MyDocumentsPage {
                     "ELIG-001-01 MA QHP English Mail", "ELIG-001-01 QHP English Mail", "ELIG-001-01 Gain of Tribal Status English 1 Mail",
                     "ELIG-001-01 Gain of Tribal Status English 2 Mail", "ELIG-001-01 Spanish Mail", "ELIG-001-01 NO CO RES Spanish Mail",
                     "ELIG-001-01 MA QHP Spanish Mail", "ELIG-001-01 QHP Spanish Mail", "ELIG-001-01 Gain of Tribal Status Spanish 1 Mail",
-                    "ELIG-001-01 Gain of Tribal Status Spanish 2 Mail", "AM-011-02 English", "AM-011-02 Spanish Mail","ELIG-001-01 MVR English 1", "ELIG-001-01 AIAN MVR Valid SEP English 1":
+                    "ELIG-001-01 Gain of Tribal Status Spanish 2 Mail", "AM-011-02 English", "AM-011-02 Spanish Mail","ELIG-001-01 MVR English 1",
+                    "ELIG-001-01 AIAN MVR Valid SEP English 1", "ELIG-001-01 AIAN MVR Valid SEP Spanish 1":
                     pdf.validateEntirePDF(docType);
             break;
             default: Assert.fail("Illegal argument Exception: Notice Name Incorrect");
